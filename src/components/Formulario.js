a4ff277f0de760b429d797db16b816e55d788a0b
manu <manu.villar.vitoria@gmail.com>
2020-06-04 15:24:45 +0200

    creating the form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,39 @@ import React from 'react';
 
 const Formulario = () => {
     return ( 
-        <h1>Formulario aquí</h1>
+        <form
+            className="col-12"
+        >
+            <fieldset className="text-center">
+                <legend>Busca de bebidas por Categoría e Ingrediente</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input 
+                        name="nombre"
+                        className="form-control"
+                        type="text"
+                        placeholder="Busca por Ingrediente"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        name="categoria"
+                        className="form-control"
+                    >
+                        <option value="">-- Selecciona Categoría --</option>
+                    </select>
+                </div>
+                <div className="col-md-4">
+                    <input 
+                        name="nombre"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
      );
 }
  
