f6408ab9c89c8a933b755e1ff2c73741041d33ab
manu <manu.villar.vitoria@gmail.com>
2020-06-05 16:49:22 +0200

    displaying the information in the Modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -42,7 +42,8 @@ const Receta = ({receta}) => {
     }
 
     //extraer los valores del context
-    const {guardarIdReceta} = useContext(ModalContext);
+    const {informacion, guardarIdReceta, guardarReceta} = useContext(ModalContext);
+    console.log(informacion);
 
 
     return (  
@@ -69,11 +70,17 @@ const Receta = ({receta}) => {
                         open={open}
                         onClose={() => {
                             guardarIdReceta(null);
+                            guardarReceta({});
                             handleClose();
                         }}
                     >
                         <div style={modalStyle} className={classes.paper}>
-                            <h1>Desde Modal</h1>
+                            <h2>{informacion.strDrink}</h2>
+                            <h3 className="mt-4">Instrucciones</h3>
+                            <p>
+                                {informacion.strInstructions}
+                            </p>
+                            <img className="img-fluid my-4" src={informacion.strDrinkThumb}/>
                         </div>
                     </Modal>
                 </div>
